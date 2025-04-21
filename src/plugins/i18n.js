e6053c77ef8a57a75c9b5443bd7da1e97de7ef94
AMRWERY <amrmounir2@gmail.com>
2025-04-21 13:33:31 +0200

    handle make ar is default locale, but user still can toggle locales

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -3,13 +3,14 @@ import en from "@/locales/en.json";
 import ar from "@/locales/ar.json";
 
 function loadLocaleMessages() {
-  const locales = [{ en: en }, { ar: ar }];
-  const messages = {};
-  locales.forEach((lang) => {
-    const key = Object.keys(lang);
-    messages[key] = lang[key];
-  });
-  return messages;
+  // const locales = [{ en: en }, { ar: ar }];
+  // const messages = {};
+  // locales.forEach((lang) => {
+  //   const key = Object.keys(lang);
+  //   messages[key] = lang[key];
+  // });
+  // return messages;
+  return { en, ar };
 }
 
 const datetimeFormats = {
@@ -100,7 +101,8 @@ const numberFormats = {
 
 export default createI18n({
   legacy: false,
-  locales: ["en", "ar"],
+  locale: "ar",
+  // locales: ["en", "ar"],
   fallbackLocale: "en",
   messages: loadLocaleMessages(),
   datetimeFormats,
