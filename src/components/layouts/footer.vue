89c78ac401d6d0b05f17f5efcb426817b850f521
AMRWERY <amrmounir2@gmail.com>
2025-04-29 23:29:35 +0300

    handle adding social media icons to footer & mobile number & address

--- src/components/layouts/footer.vue
+++ src/components/layouts/footer.vue
@@ -19,9 +19,9 @@
                             <iconify-icon icon="heroicons-outline:location-marker" width="20" height="20"
                                 class="flex-shrink-0 me-3"></iconify-icon>
                             <div>
-                                <p>123 Business Street</p>
-                                <p>New York, NY 10001</p>
-                                <p>United States</p>
+                                <p>{{ $t('footer.pyramids_gardens') }}</p>
+                                <p>{{ $t('footer.gize') }}</p>
+                                <p>{{ $t('footer.egypt') }}</p>
                             </div>
                         </div>
                     </div>
@@ -32,13 +32,38 @@
                         <div class="flex items-center">
                             <iconify-icon icon="heroicons-outline:phone" width="20" height="20"
                                 class="flex-shrink-0 me-3"></iconify-icon>
-                            <a href="tel:+1234567890" class="transition-colors hover:text-white">+1 (234) 567-890</a>
+                            <a href="tel:+201006312544"
+                                class="transition-colors hover:text-white rtl:rotate-0 text-start inline-block [direction:ltr] [unicode-bidi:isolate]">
+                                +20 100 631 2544
+                            </a>
                         </div>
                     </div>
                 </div>
 
                 <!-- Copyright -->
                 <div class="pt-8 mt-8 text-center border-t border-gray-800">
+                    <div class="flex justify-center mb-5 space-s-6">
+                        <a href="https://www.facebook.com/BRAND.SHOP250" target="_blank"
+                            class="w-8 h-8 border-none outline-none">
+                            <span class="sr-only">Facebook</span>
+                            <img src="/facebook.svg" alt="facebook-icon" class="w-full h-full">
+                        </a>
+                        <a href="https://www.instagram.com/3mr_wery/" target="_blank"
+                            class="w-8 h-8 border-none outline-none">
+                            <span class="sr-only">Instagram</span>
+                            <img src="/instagram.svg" alt="instagram-icon" class="w-full h-full">
+                        </a>
+                        <a href="https://www.snapchat.com/add/dummy-account123" target="_blank"
+                            class="w-8 h-8 border-none outline-none">
+                            <span class="sr-only">Snapchat</span>
+                            <img src="/snapchat.svg" alt="snapchat-icon" class="w-full h-full">
+                        </a>
+                        <a href="https://www.tiktok.com/@dummy_tiktok_account" target="_blank"
+                            class="w-8 h-8 text-gray-100 border-none outline-none">
+                            <span class="sr-only">Tiktok</span>
+                            <img src="/tiktok-outline.svg" alt="tiktok-icon" class="w-full h-full">
+                        </a>
+                    </div>
                     <p class="text-sm">&copy; 2025 Brand Shop. {{ $t('footer.all_rights_reserved') }}</p>
                 </div>
             </div>
--- src/components/whatsapp-componenet.vue
+++ src/components/whatsapp-componenet.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <a href="https://wa.me/201006312544" target="_blank" role="button"
-            class="fixed z-50 flex items-center justify-center py-3.5 px-3.5 bg-[#25D366] text-white transition-all duration-300 rounded-full shadow-lg bottom-36 end-8 focus:ring-4 active:scale-90">
-            <iconify-icon icon="ic:twotone-whatsapp" width="30" height="30"></iconify-icon>
+            class="fixed z-50 flex items-center justify-center py-3.5 px-3.5 bg-[#25D366] text-white transition-all duration-300 rounded-full shadow-lg bottom-[150px] end-8 focus:ring-4 active:scale-90">
+            <img src="/whatsapp.svg" alt="whatsapp-icon" class="w-[30px] h-[30px]">
         </a>
     </div>
 </template>
