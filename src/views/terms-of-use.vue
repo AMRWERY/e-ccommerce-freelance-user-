bf719dfefd432c3dafa6d95c9103efe10d4d6481
AMRWERY <amrmounir2@gmail.com>
2025-05-01 16:01:21 +0300

    handle create terms-of-use page

--- src/views/terms-of-use.vue
+++ src/views/terms-of-use.vue
@@ -1,6 +1,78 @@
 <template>
     <div>
-        <p>terms-of-use</p>
+        <div class="max-w-3xl py-16 mx-auto sm:py-20">
+            <p class="mb-10 text-3xl font-semibold text-center">{{ $t('footer.terms_of_use') }}</p>
+            <div class="space-y-8">
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_one')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_one') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_two') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_two')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_three') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_four') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_five') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_six') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_seven') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_three')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_eight') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_four')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_nine') }}</p>
+                        <p>{{ $t('footer.terms_of_use_content_ten') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_five')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_eleven') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_six')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_content_twelve') }}</p>
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <span class="text-xl font-semibold leading-relaxed text-gray-800">{{
+                        $t('footer.terms_of_use_title_seven')
+                        }}</span>
+                    <div class="space-y-2 font-medium">
+                        <p>{{ $t('footer.terms_of_use_title_thirteen') }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
